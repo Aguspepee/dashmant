94d70d30da840e920649c3ec414d9ae78169c68c
Aguspepee <sanchezagustinezequiel@gmail.com>
2022-05-27 10:15:51 -0300

    Agregado de tabla

---
+++ src/Utils/Datos_Gestion.js
@@ -0,0 +1,304 @@
+module.exports={
+    Datos_Gestion:[
+        { Tipo: "GESTIÓN DE ACEITES" },
+        {
+          Tipo: "ZN",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZS",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZO",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZA",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        { Tipo: "MANTENIMIENTO ESTACIONES" },
+        {
+          Tipo: "ZN",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZS",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZO",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZA",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        { Tipo: "MANTENIMIENTO LÍNEAS" },
+        {
+          Tipo: "ZN",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZS",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZO",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+        {
+          Tipo: "ZA",
+          RPM_2020: "Plan Estacional",
+          Generadas_2020: "207",
+          Cerradas_2020: "137",
+          RPM_2021: "Plan Estacional",
+          Generadas_2021: "207",
+          Cerradas_2021: "137",
+          RPM_2022: "Plan Estacional",
+        },
+      ],
+
+      
+      Datos_Columnas:[
+        {
+          Header: "                ",
+          columns: [
+            {
+              Header: "     ",
+              columns: [
+                {
+                  Header: "        ",
+                  accessor: "Tipo",
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  minWidth: 110,
+                  backgroundColor: "gray",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          Header: "PERIODO 2020",
+          columns: [
+            {
+              Header: " ",
+              columns: [
+                {
+                  Header: "RPM",
+                  accessor: "RPM_2020",
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  minWidth: 110,
+                },
+                {
+                  Header: "Generadas",
+                  accessor: "Generadas_2020",
+                  fontSize: "12px",
+                  minWidth: 60,
+                  //backgroundColor: "gray",
+                },
+                {
+                  Header: "Cerradas",
+                  accessor: "Cerradas_2020",
+                  fontSize: "12px",
+                  minWidth: 60,
+                  //backgroundColor: "gray",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          Header: "PERIODO 2021",
+          columns: [
+            {
+              Header: "  ",
+              columns: [
+                {
+                  Header: "RPM",
+                  accessor: "RPM_2021",
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  minWidth: 110,
+                },
+                {
+                  Header: "Generadas",
+                  accessor: "Generadas_2021",
+                  fontSize: "12px",
+                  minWidth: 60,
+                  //backgroundColor: "gray",
+                },
+                {
+                  Header: "Cerradas",
+                  accessor: "Cerradas_2021",
+                  fontSize: "12px",
+                  minWidth: 60,
+                  //backgroundColor: "gray",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          Header: "PERIODO 2022",
+          columns: [
+            {
+              Header: "   ",
+              columns: [
+                {
+                  Header: "RPM",
+                  accessor: "RPM_2022",
+                  fontSize: "12px",
+                  fontWeight: "bold",
+                  minWidth: 110,
+                },
+              ],
+            },
+            {
+              Header: "ene-22",
+              columns: [
+                {
+                  Header: "Generadas",
+                  accessor: "Generadas_2022_1",
+                  minWidth: 70,
+                },
+                {
+                  Header: "Cerradas",
+                  accessor: "Cerradas_2022_1",
+                  minWidth: 70,
+                },
+              ],
+            },
+            {
+              Header: "feb-22",
+              columns: [
+                {
+                  Header: "Generadas",
+                  accessor: "Generadas_2022_2",
+                  minWidth: 70,
+                },
+                {
+                  Header: "Cerradas",
+                  accessor: "Cerradas_2022_2",
+                  minWidth: 70,
+                },
+              ],
+            },
+            {
+              Header: "mar-22",
+              columns: [
+                {
+                  Header: "Generadas",
+                  accessor: "Generadas_2022_3",
+                  minWidth: 70,
+                },
+                {
+                  Header: "Cerradas",
+                  accessor: "Cerradas_2022_3",
+                  minWidth: 70,
+                },
+              ],
+            },
+            {
+              Header: "abr-22",
+              columns: [
+                {
+                  Header: "Generadas",
+                  accessor: "Generadas_2022_4",
+                  minWidth: 70,
+                },
+                {
+                  Header: "Cerradas",
+                  accessor: "Cerradas_2022_4",
+                  minWidth: 70,
+                },
+              ],
+            },
+            {
+              Header: "may-22",
+              columns: [
+                {
+                  Header: "Generadas",
+                  accessor: "Generadas_2022_5",
+                  minWidth: 70,
+                },
+                {
+                  Header: "Cerradas",
+                  accessor: "Cerradas_2022_5",
+                  minWidth: 70,
+                },
+              ],
+            },
+          ],
+        },
+      ]
+
+}
